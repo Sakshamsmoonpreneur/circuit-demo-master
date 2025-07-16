03128b00552011dad443b9459e973ebdc0f02826
Krish Renjen <krish.renjen@gmail.com>
2025-07-16 14:40:31 -0400

    fixed ui issue for pop up editor

--- src/components/code/PopupEditor.tsx
+++ src/components/code/PopupEditor.tsx
@@ -19,12 +19,13 @@ export default function PopupEditor({
     <div className="fixed top-10 left-10 right-10 bottom-10 bg-white z-50 border p-4 shadow-lg">
       <button
         onClick={onClose}
-        className="absolute top-2 right-2 text-white bg-red-500 px-2 py-1 rounded"
+        className="absolute top-2 right-2 text-white bg-red-500 px-2 py-1 rounded z-50"
       >
         Close
       </button>
       <Editor
-        height="90%"
+        height="99%"
+        width="95%"
         defaultLanguage="python"
         value={code}
         theme="vs-dark"
